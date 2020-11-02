771ba45531de250447bb9b7a38ab86ffa73fb6dc
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2020-11-02 23:03:25 +0600

    week2_lecture2

---
+++ Lecture2.py
@@ -0,0 +1,56 @@
+# prod1 = "Iphone"
+# prod2 = "Samsung"
+
+# karzina = [prod1, prod2,1, 2, 3]
+
+
+
+# karzina = [1, [2, 3, 4, [5, 6, 7]], 3, 4]
+# # nums.append(6)
+# print(nums)
+# nums.pop()
+# print(nums[1][3][1]) # geteregennost spiskov/slojniye spiski
+# spisok1 = [1, 2, 3]
+# name = "Bakyt"
+# spisok2 = list(name)
+# print(spisok2)
+
+# from copy import deepcopy
+# spisok = ["name", "makers", True,2, [3, 4, 5], 2, "name, 2"]
+# spisok2 = [0, 9, 8, 7, 6, "word1"]
+# name = 2
+# spisok.append("word")
+# new_spisok = spisok.copy()
+# new_spisok[4][0] = "new word"
+# print(new_spisok)
+# print(spisok)
+# new_spisok = spisok.copy()
+# spisok[3] = "word"
+# print(spisok)
+# print(new_spisok)
+# new_spisok = deepcopy(spisok)
+# spisok[4][1] = "word"
+# print(spisok)
+# print(new_spisok)
+# num = spisok.count("name")
+# print(num)
+# spisok.extend(spisok2)
+# spisok3 = spisok2 + spisok
+# print(spisok3)
+# num = spisok.index(2, 4)
+# num = spisok.index("name")
+# print(num)
+# spisok.insert(3, "indexed")
+# print(spisok)
+# item = spisok.pop(1)       pop udalyaet s konsa i vozvrashaet po indeksu
+# print(item)
+# spisok.remove("name")         # udalyet po obiektu
+# spisok.reverse()       perevarachivaet s konsa
+# spisok = [1, 3, 4, 6, 6, 7, 9, 3, 1]
+# spisok = ["abc", "bcd", "sfdsf", "dedsa"]
+# spisok.sort(key=len, reverse=True)
+
+# print(spisok)               # malenkiyie bukvy bolshe chem bolshiey
+# print(spisok[1:3])
+
+
